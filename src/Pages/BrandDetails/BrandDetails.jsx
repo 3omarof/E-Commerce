561bf369d09098baa9bd353fe6e4f53925e3dc28
3omarof <omartamer12335@gmail.com>
2025-07-09 18:11:09 +0300

    Fix: corrected Card import in BrandDetails

--- src/Pages/BrandDetails/BrandDetails.jsx
+++ src/Pages/BrandDetails/BrandDetails.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import Loading from "../../Components/Loading/Loading";
-import Card from "../../Components/card/Card";
+import Card from "../../Components/Card/Card";
 import noProduct from "../../assets/imgs/product-not-found.jpg";
 
 export default function BrandDetails() {
